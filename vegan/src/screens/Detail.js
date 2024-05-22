9363e9a8eff1f59c67fd0a093b92de582a42743e
rafael-men <rafaelgoncalves1822@gmail.com>
2024-05-22 19:06:11 -0300

    navigation done

---
+++ vegan/src/screens/Detail.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Detail = () => {
+  return (
+    <View style={{justifyContent:'center',alignItems:'center',flex:1}}>
+      <Text>Detail</Text>
+    </View>
+  )
+}
+
+export default Detail
